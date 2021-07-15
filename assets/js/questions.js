de273e4d5896d7ff8154d2a5d566b88c99ffd872
DHDH <damon.ha6@gmail.com>
2021-07-15 00:09:51 -0400

    add questions and css

---
+++ assets/js/questions.js
@@ -0,0 +1,31 @@
+var questions = [
+    {
+      title: "1. What does HTML stand for?",
+      multiChoice: ["A. Hyper Trainer Marking Language", "B. Hyper Text Marketing Language", "C. Hyper Text Markup Language", "D. Hyper Text Markup Leveler"],
+      answer: "C. Hyper Text Markup Language"
+    },
+  
+    {
+      title: "Which of the following is not a type of computer programming language?",
+      multiChoice: ["A. Java", "B. C++", "C. Pyton", "D. Mozila"],
+      answer: "D. Mozila"
+    },
+  
+    {
+      title: "Properties in a JavaScript object are often refferred to as what?",
+      multiChoice: ["A. dot walking", "B. key-value pairs", "C. nested properties", "D. undefined"],
+      answer: "B. key-value pairs"
+    },
+  
+    {
+      title: "External stylesheets are stored in what type of files?",
+      multiChoice: ["A. CSS", "B. HTML", "C. PHP", "D. XML"],
+      answer: "A. CSS"
+    },
+  
+    {
+      title: "What is the format called that is used for storing and transporting data??",
+      multiChoice: ["A. JSON", "B. Syntax", "C. Font", "D. HTML"],
+      answer: "A. JSON"
+    }
+  ];
